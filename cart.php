06652baa6d47a2cf7834d1c1c3a96bbd983b1a8b
Nikita-Voronov <omgtikalol@gmail.com>
2020-11-16 17:42:42 +0200

    First commit

---
+++ cart.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+include 'class_cart.php';
+$a = new Cart();
+$iditem = $_GET['products'];
+$a->add($_GET['products'], $_GET['quantity'], $products[$iditem]['price']);
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Ваша корзина</title>
+</head>
+<body>
+<div style="color: #fc9a02">
+    К оплате <?php echo $a->sum; ?><br><br>
+    <table>
+        <?php
+        foreach ($a->cart['items'] as $key => $items) {
+            echo '<tr><td>' . 'id товара:' . $iditem . '</td><td>'
+                . 'Наиминование товара:' . $products[$iditem]['name'] . '</td><td>' . 'Количество:' . $_GET['quantity'] . '</td><td><a href=/delete.php?id = ' . $key . '>Удалить</a></td ></tr>';
+        }
+        ?>
+    </table>
+
+    <form action="" method="GET">
+
+        <select name="products">
+            <option value="0">Choose your items</option>
+            <?php
+            foreach ($products as $key => $products) {
+                echo '<option value="' . $key . '">' . $products['name'] . '</option>';
+            }
+            ?>
+        </select><br>
+
+
+        Количество:<br>
+        <input name="quantity" type="text"><br>
+        <input type="submit" name="" value="Get to cart">
+
+    </form>
+
+
+</div>
+
+</body>
+</html>
---
+++ class_cart.php
@@ -0,0 +1,104 @@
+<?php
+session_start();
+
+$products = [
+    4 => ['name' => 'Книга', 'price' => 100],
+    12 => ['name' => 'Диск', 'price' => 50],
+    32 => ['name' => 'Флешка', 'price' => 240]
+];
+
+class Cart
+{
+    //атрибуты
+    public $cart;
+    public $items;//элементы корзины
+    public $sum;//сумма элементов в корзине
+    public $discount;//сумма с учетом скидки
+    public $count;//количество элементов
+
+    //метод
+    public function __construct()
+    {
+        $this->cart = $_SESSION['cart'];
+        $this->setCount();
+        $this->setDiscount();
+        $this->setItems();
+        $this->setSum();
+        $this->calc();
+    }
+
+    public function add($id, $quantity, $price)
+    {
+        if (empty($this->cart)) {
+            $this->cart['items'][] = ['id' => $id, 'quantity' => $quantity, 'price' => $price * $quantity];
+        } else {
+            foreach ($this->cart['items'] as $key => $value) {
+                if ($value['id'] == $id) {
+                    $this->cart['items'][$key]['quantity'] += $quantity;
+                    $this->cart['items'][$key]['price'] *= $this->cart['items'][$key]['quantity'];
+                }
+            }
+        }
+    }
+    public function calc()
+    {
+        $this->sum = 0;
+        $this->count = 0;
+        $this->discount = 0;
+
+        foreach ($this->cart['items'] as $key => $value) {
+            $this->sum += $value['price'];
+            $this->count += $value['quantity'];
+        }
+        if ($this->count < 10 && $this->sum > 2000) {
+            $this->discount = $this->sum * 0.07;
+            $this->sum = $this->sum - $this->discount;
+        } elseif ($this->count> 10) {
+            $this->discount = $this->sum * 0.1;
+            $this->sum = $this->sum - $this->discount;
+        }
+        return ;
+    }
+
+    public function setSum()
+    {
+        $this->sum = $this->cart['sum'];
+    }
+
+    public function setItems()
+    {
+        $this->items = $this->cart['items'];
+    }
+
+    public function setDiscount()
+    {
+        $this->discount = $this->cart['discount'];
+    }
+
+    public function setCount()
+    {
+        $this->count = $this->cart['count'];
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    public function getSum()
+    {
+        return $this->sum;
+    }
+
+    public function getDiscount()
+    {
+        return $this->discount;
+    }
+
+    public function getCount()
+    {
+        return $this->count;
+    }
+}
+
+?>
